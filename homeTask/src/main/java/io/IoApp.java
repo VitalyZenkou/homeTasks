97774cea75e2d5664e731d48153ea75405709abb
VitalyZenkou <blink812@gmail.com>
2019-02-20 00:58:39 +0300

    IO home task

---
+++ homeTask/src/main/java/io/IoApp.java
@@ -0,0 +1,11 @@
+package io;
+
+import io.util.IOUtil;
+
+public class IoApp {
+
+    public static void main(String[] args){
+        IOUtil.findAmountLetters();
+        IOUtil.sortNumberFromFile(100);
+    }
+}
